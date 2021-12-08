3c08f32df96a9ff9dda9cf0224531c08d331df59
hamazSabir <sabirha00@gmail.com>
2021-12-08 22:36:11 +0100

    fixes 4

--- app.js
+++ app.js
@@ -33,13 +33,14 @@ app.use("/api",cart);
 
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("/frontend/build"));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-    })
-
-}
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('./frontend/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 3001;
 
